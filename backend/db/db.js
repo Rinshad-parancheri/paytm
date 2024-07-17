7f9d53467be05e95633db5c32203c9b7a0906178
Rinshad-parancheri <rinshadp014@gmail.com>
2024-07-17 06:45:49 +0530

    add user schema

--- backend/db/db.js
+++ backend/db/db.js
@@ -4,7 +4,28 @@ const dbUrl = process.env.DB_URL
 
 mongoose.connect(url)
 const userSchema = new mongoose.Schema({
-
+  firstName: {
+    type: String,
+    required: [true, 'First name is required'],
+    trim: true
+  },
+  lastName: {
+    type: String,
+    required: [true, 'Last name is required'],
+    trim: true
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    unique: true,
+    lowercase: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is required'],
+    minlength: [8, 'Password must be at least 8 characters long']
+  },
 })
 
 
