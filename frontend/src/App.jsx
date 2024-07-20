3292e0ceb9a18c2a1025ef7fa82cf60258bd4b66
Rinshad-parancheri <rinshadp014@gmail.com>
2024-07-20 08:41:17 +0530

    add signin n signup page
    learn about the reacter-router-dom and taliwindcss properties

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,21 @@
 import { useState } from 'react'
 import './App.css'
+import { BrowserRouter } from 'react-router-dom'
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
-  return
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
 }
 
 
---
+++ frontend/src/pages/Signup.jsx
@@ -0,0 +1,30 @@
+import { BottomWarning } from "../components/BottomWarning"
+import { Button } from "../components/Button"
+import { Heading } from "../components/Heading"
+import { InputBox } from "../components/InputBox"
+import { Subheading } from "../components/SubHeading"
+
+export const Signin = () => {
+  return (
+    <div className="bg-slate-300 h-screen">
+      <div className="flex flex-col justify-center ">
+        <div className="rounded-l bg-white w-80 text-center p-2 h-max px-4">
+          <Heading label={"Sign in"}></Heading>
+          <Subheading label={"Enter your credentials to signin"}></Subheading>
+          <InputBox label={"First Name"} placeholder={"jan doe"}></InputBox>
+          <InputBox label={"Second Name"} placeholder={"Doe"}></InputBox>
+          <InputBox label={"Email"} placeholder={"dapper948@gmail"}></InputBox>
+          <InputBox label={"Password"} placeholder={"2f9f8fl"}></InputBox>
+          <Button label={"Sign In"}></Button>
+          <BottomWarning label={"Alreday have an account"} btnText={"Sign in"} to={"/signin"}></BottomWarning>
+        </div>
+      </div>
+    </div>
+
+  )
+
+
+}
+
+
+
